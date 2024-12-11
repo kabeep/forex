432a30f9a7e1566b2bfe0f9b5e6c6a12aa08d6a5
kabeep <zzx2067@outlook.com>
2024-12-11 13:23:16 +0800

    chore: update typedoc config

--- typedoc.config.js
+++ typedoc.config.js
@@ -4,9 +4,9 @@ module.exports = {
     sort: ['source-order'],
     projectDocuments: ['./README.*.md', 'LICENSE'],
     navigationLinks: {
-        Docs: 'https://kabeep.github.io/zodiac-animals/',
-        GitHub: 'https://github.com/kabeep/zodiac-animals',
-        NPM: 'https://www.npmjs.com/package/@kabeep/zodiac-animals',
+        Docs: 'https://kabeep.github.io/forex/',
+        GitHub: 'https://github.com/kabeep/forex',
+        NPM: 'https://www.npmjs.com/package/@kabeep/forex',
     },
     plugin: [
         'typedoc-material-theme',
@@ -18,7 +18,7 @@ module.exports = {
         'typedoc-plugin-rename-documents',
         'typedoc-plugin-version-header',
     ],
-    themeColor: '#a2cf6e',
+    themeColor: '#6600EB',
     renameDocuments: {
         './README.zh-CN.md': '简体中文',
     },
